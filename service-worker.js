8bd95877137a31e34659ebe32245451a0f56c9e2
Microgym <miguel.ferrer@rinfresco.es>
2026-09-21 12:56:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='mis-tareas-pwa-v47';
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./fondo-aventura.png", "./heroe-original.png", "./objetivo-esqui.png", "./familia-aventura.png", "./nina.png", "./nino.png",'./ciudad-valencia.png','./ciudad-madrid.png'];
+const CACHE='mis-tareas-pwa-v48';
+const ASSETS=['./objetivo-esqui-nina.png',"./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./fondo-aventura.png", "./heroe-original.png", "./objetivo-esqui.png", "./familia-aventura.png", "./nina.png", "./nino.png",'./ciudad-valencia.png','./ciudad-madrid.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
