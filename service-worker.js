b2ef2c969d6dad98b907aa221bcafb0a9a875fb8
Microgym <miguel.ferrer@rinfresco.es>
2026-09-21 14:35:32 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,5 @@
-const CACHE_CLEANUP='mis-tareas-cleanup-v53';
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil((async()=>{
-    const keys=await caches.keys();
-    await Promise.all(keys.map(k=>caches.delete(k)));
-    await self.clients.claim();
-    const reg=await self.registration;
-    await reg.unregister();
-  })());
-});
-// Sin listener fetch: la app siempre carga desde la red.
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil((async()=>{
+ const ks=await caches.keys(); await Promise.all(ks.map(k=>caches.delete(k)));
+ await self.registration.unregister(); await self.clients.claim();
+})()));
