37b32a2284ccfd1c81bbaf0e8289ee5078a3fb08
Microgym <miguel.ferrer@rinfresco.es>
2026-09-21 16:01:51 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mis-tareas-pwa-v60';
+const CACHE='mis-tareas-pwa-v61';
 const ASSETS=[
 './','./index.html','./manifest.webmanifest',
 './icon-180.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png',
