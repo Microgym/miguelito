865c86fb33fef4632da8c3ca61e3b657d417e652
Microgym <miguel.ferrer@rinfresco.es>
2026-09-21 14:58:55 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,25 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{
- const ks=await caches.keys(); await Promise.all(ks.map(k=>caches.delete(k)));
- await self.registration.unregister(); await self.clients.claim();
-})()));
+const CACHE='mis-tareas-pwa-v59';
+const ASSETS=[
+'./','./index.html','./manifest.webmanifest',
+'./icon-180.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png',
+'./hero-boy-valencia.webp','./hero-boy-madrid.webp',
+'./hero-girl-valencia.webp','./hero-girl-madrid.webp',
+'./ski-boy.webp','./ski-girl.webp'
+];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request,{cache:'no-store'}).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
+});
